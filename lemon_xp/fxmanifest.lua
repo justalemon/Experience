1215ab6a7a3c3e760af63d859b065d4892097ec9
Hannele Ruiz <justlemoncl@gmail.com>
2024-09-05 21:29:22 -0400

    Added basic set of files

---
+++ lemon_xp/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "cerulean"
+games { "gta5", "rdr3" }
+
+author "Lemon <lemon@itslemonchan.com>"
+description "Simple experience system for FiveM/RedM"
+version "1.0.0"
+
+server_script "server.lua"
+client_script "client.lua"
