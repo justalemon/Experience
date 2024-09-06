a17b0c9f0f5df3b5292f846bdf3382ba7df358bb
Hannele Ruiz <justlemoncl@gmail.com>
2024-09-05 21:55:52 -0400

    Added detection of storage methods

--- lemon_xp/server.lua
+++ lemon_xp/server.lua
@@ -1,3 +1,16 @@
+local storage = (function ()
+    local method = GetConvar("lemon_xp_storage", "")
+
+    if method ~= "" then
+        return method
+    end
+
+    if GetResourceState("oxmysql") == "started" then
+        return "oxmysql"
+    end
+    return "json"
+end)()
+
 local function init()
     if GetCurrentResourceName() ~= "lemon_xp" then
         print("ERROR! Experience script should be called lemon_xp, current name is " .. GetCurrentResourceName())
