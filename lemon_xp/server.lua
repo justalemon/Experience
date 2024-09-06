4491e8f806abb5b426f76c4918e87f1a413103e8
Hannele Ruiz <justlemoncl@gmail.com>
2024-09-05 22:53:04 -0400

    Added exports to add and remove xp

--- lemon_xp/server.lua
+++ lemon_xp/server.lua
@@ -12,6 +12,51 @@ local storage = (function ()
 end)()
 local cache = {}
 
+local function change(src, amount)
+    src = tonumber(src)
+
+    local identifier = GetPlayerIdentifierByType(src, "license")
+    local total = (cache[src] or 0) + amount
+
+    if total < 0 then
+        total = 0
+    end
+
+    cache[src] = total
+
+    if storage == "json" then
+        error("not implemented")
+    elseif storage == "oxmysql" then
+        if GetResourceState("oxmysql") ~= "started" then
+            error("Storage method set to oxmysql, but oxmysql is not running")
+        end
+
+        exports.oxmysql:prepare_async("INSERT INTO xp (id, xp) VALUES (?, ?) ON DUPLICATE KEY UPDATE xp = VALUES(XP)", {identifier, total})
+    end
+end
+
+local function add(src, amount)
+    src = tonumber(src)
+
+    if amount <= 0 then
+        error("Attempted to add experience equal or under zero: " .. tostring(amount))
+    end
+
+    change(src, math.abs(amount))
+end
+exports("add", add)
+
+local function remove(src, amount)
+    src = tonumber(src)
+
+    if amount <= 0 then
+        error("Attempted to add experience equal or under zero: " .. tostring(amount))
+    end
+
+    change(src, -amount)
+end
+exports("remove", remove)
+
 local function clientReady()
     local src = tonumber(source)
 
