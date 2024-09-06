a2480c508e301a2d0f5d0bb0b8487957af44c12b
Hannele Ruiz <justlemoncl@gmail.com>
2024-09-06 01:38:05 -0400

    Added a couple of extra metadata fields
    Inspired by oxmysql

--- lemon_xp/fxmanifest.lua
+++ lemon_xp/fxmanifest.lua
@@ -1,9 +1,12 @@
 fx_version "cerulean"
 games { "gta5", "rdr3" }
 
+name "Lemon Experience (lemon_xp)"
 author "Lemon <lemon@itslemonchan.com>"
 description "Simple experience system for FiveM/RedM"
 version "1.0.0"
+repository "https://github.com/justalemon/Experience"
+license "LGPL-3.0-or-later"
 
 server_script "server.lua"
 client_script "client.lua"
